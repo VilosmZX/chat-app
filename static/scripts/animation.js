e53cf90eb3bcdb5901da280771cc0e0f46d8c1fd
Joshua <jravaell@gmail.com>
2022-07-01 19:35:25 +0700

    added modal

---
+++ static/scripts/animation.js
@@ -0,0 +1,11 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+const playerImg = new Image();
+playerImg.src = 'images/mario.png';
+let x = 0;
+
+const animate = () => {
+}
+
+animate();
